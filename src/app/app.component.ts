b6c628e649491db94caaf6d23e39e32692bd5a64
Jose Torres <vidojam@gmail.com>
2023-08-18 15:07:01 -0400

    first angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// export class AppComponent {
+//   title: string;
+//   numberOne: number = 1;
+//   numberTwo: number = 2;
+//   togglePokemon: boolean = true;
+//   constructor() {
+//     this.title = "jose"
+//   }
+// }
+
+// export class AppComponent {
+//   title: string = "basket of baby beavers";
+//   imgSrc: string = "https://cwh1.vet.cornell.edu/system/file/baby-beavers-6-jpg";
+  
+//   constructor() {
+    
+//   }
+// }
+
 export class AppComponent {
-  title = 'my-first-project';
+  favoriteAnimal: string = "turtle";
+  
+  constructor() {
+    
+  }
 }
